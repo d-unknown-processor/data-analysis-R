49481e659c8236d94e0cd744facbe0ae25e457c0
Mateusz Czerwiński <mtczerwinski@gmail.com>
2012-11-18 20:54:45 +0100

    project 1

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-data-analysis-R
-===============
-
 Data analysis with R 
+=====================
+
 
 Books/Notes
 * [The R Book](http://www.amazon.com/The-Book-Michael-J-Crawley/dp/0470510242)
 * [Intro to Spatial Statistics in R](http://scc.stat.ucla.edu/page_attachments/0000/0094/spatial_R_1_09S.pdf)
+* [Wprowadzenie do środowiska R](http://cran.r-project.org/doc/contrib/Komsta-Wprowadzenie.pdf)
---
+++ statistics/homework_1/homework_1.R
@@ -0,0 +1,51 @@
+# zapisujemy dane z pliku csv do data.frame
+dane <- read.csv("homework_1_data.csv", sep = ',')
+
+
+# wydobywamy "wartość spółek" z data.frame
+dane_wartosc <- dane[,2]
+
+# wydobywamy "cena do zysk" z data.frame
+dane_cena_do_zysk <- dane[,3]
+
+
+# liczymy kwantyle, szóstego typu
+kwantyle <- quantile(dane_wartosc, type = 6)
+
+bardzo_male <- subset(dane, Wartość <= as.numeric(kwantyle[2]))
+
+male <- subset(dane, Wartość > as.numeric(kwantyle[2]) & Wartość <= as.numeric(kwantyle[3]))
+
+srednia <- subset(dane, Wartość > as.numeric(kwantyle[3]) & Wartość <= as.numeric(kwantyle[4]))
+
+duze <- subset(dane, Wartość > as.numeric(kwantyle[4]))
+
+# tworzymy macierz 3x4 i wypełniamy ją zerami
+# następnie wpiszemy do niej wyniki
+# dalszych obliczeń
+wynik <- matrix(0, nrow = 3, ncol = 4)
+
+# następujące obliczenia są oczywiste
+wynik[1,1] <- round(mean(bardzo_male[,3]), digit = 2)
+wynik[2,1] <- round(median(bardzo_male[,3]), digit = 2)
+wynik[3,1] <- round(sd(bardzo_male[,3]), digit = 2)
+
+
+wynik[1,2] <- round(mean(male[,3]), digit = 2)
+wynik[2,2] <- round(median(male[,3]), digit = 2)
+wynik[3,2] <- round(sd(male[,3]), digit = 2)
+
+
+wynik[1,3] <- round(mean(srednia[,3]), digit = 2)
+wynik[2,3] <- round(median(srednia[,3]), digit = 2)
+wynik[3,3] <- round(sd(srednia[,3]), digit = 2)
+
+
+wynik[1,4] <- round(mean(duze[,3]), digit = 2)
+wynik[2,4] <- round(median(duze[,3]), digit = 2)
+wynik[3,4] <- round(sd(duze[,3]), digit = 2)
+
+# i podsumowanie jako date.frame
+podsumowanie <- as.data.frame(wynik)
+colnames(podsumowanie) <- c("bardzo małe", "małe", "średnie", "duże")
+row.names(podsumowanie) <- c("mean", "median", "sd")
